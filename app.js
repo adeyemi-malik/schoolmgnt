71c486250adf365c42956f1bb864aea92dd99aff
olaitankareem01 <olaitankareem01@gmail.com>
2020-09-26 16:38:22 -0700

    added start script to package.json

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const AuditLog = require('./models/auditlog.js');
 const auditLog = new AuditLog();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 app.use(express.static(__dirname + '/public'));
